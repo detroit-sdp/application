4ed186aafe7d06ce3fdfa9b19cabb1ca89d0d65e
NiuJ1ao <NiuJ1ao@github.com>
2020-01-22 19:39:52 +0000

    Create layout of AddPatient

---
+++ app/src/main/java/com/example/sdp_assistiverobot/AddPatientActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sdp_assistiverobot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPatientActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_patient)
+
+    }
+}
