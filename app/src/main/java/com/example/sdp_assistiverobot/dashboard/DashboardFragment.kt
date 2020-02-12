be83de7b5ddf6a8c63d62cd5da0390fbeb19d82a
Ching Ling Yeung <chinglingyeung1@gmail.com>
2020-02-12 12:50:09 +0000

    added components to send requests

--- app/src/main/java/com/example/sdp_assistiverobot/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/sdp_assistiverobot/dashboard/DashboardFragment.kt
@@ -1,20 +1,23 @@
 package com.example.sdp_assistiverobot.dashboard
 
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.sdp_assistiverobot.R
 import com.example.sdp_assistiverobot.patients.Patient
+import com.example.sdp_assistiverobot.patients.PatientsFragment
 import com.github.mikephil.charting.data.*
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_dashboard.*
+
 
 /**
  * A simple [Fragment] subclass.
@@ -50,9 +53,16 @@ class DashboardFragment : Fragment() {
         val button: Button = button_sendTadashi
         button.setOnClickListener {
             // Choose patient to send Tadashi to
-            Intent(this.context, ChoosePatientActivity::class.java).also {
-                startActivity((it))
-            }
+            val patientsFragment = PatientsFragment()
+            val fragmentManager: FragmentManager? = fragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager!!.beginTransaction()
+            fragmentTransaction.replace(R.id.container, patientsFragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+
+//            Intent(this.context, ChoosePatientActivity::class.java).also {
+//                startActivity((it))
+//            }
         }
     }
 
